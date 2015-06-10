855af17f4a6acfbdd7c01c7ebec1367aca0c617f
Matthias Thubauville <matthias.thubo@gmail.com>
2015-06-10 14:33:32 +0200

    README & generalization of docker_latexmk_run.sh

--- docker_latexmk_run.sh
+++ docker_latexmk_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ./docker_latexmk_run.sh <Folder to .tex file> <Name of .tex file>
-docker run --rm -v ${PWD}:/data latex sh -c "cd /data/${1} && latexmk -xelatex ${2}"
+# ./docker_latexmk_run.sh <Folder to .tex file> <command after latexmk, e.g. -xelatex file.tex>
+docker run --rm -v ${PWD}:/data latex sh -c "cd /data/${1} && latexmk ${2}"
